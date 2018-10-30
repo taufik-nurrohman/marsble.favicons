9e8d347997639b1e94236e16f12353c80fc62264
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-10-30 08:58:11 +0700

    Refactor `Favicons` Class
    + Add ability to search favicon URL from HTML page.

--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Marsble Favicons Proxy
  *
@@ -6,33 +7,41 @@
  * @copyright Marsble. MIT.
  */
 
-function is_home() {
-    return $_SERVER['REQUEST_URI'] == '/';
-}
 
 /**
- * Remove HTTP
+ * Settings
  */
-function removehttp($url)
-{
-    if (preg_replace('#^https?://#', '', $url) ) {
-        $url = preg_replace('#^https?://#', '', $url);
-    }
 
-    return $url;
+define('MARSBLE_DEBUG', isset($_GET['debug']));
+
+// <https://stackoverflow.com/q/68651/1163000>
+// <https://stackoverflow.com/a/2183140/1163000>
+// TODO: Allow underscores in domain name
+$k = array_keys(isset($_GET) ? $_GET : []);
+define('MARSBLE_URL_PATH', strtr(array_shift($k), '_', '.'));
+define('MARSBLE_URL_QUERY', http_build_query($_GET));
+
+
+/**
+ * Check Home Page
+ */
+
+function is_home() {
+    return !MARSBLE_URL_PATH;
 }
 
-if ( is_home() ) {
 
-    require dirname( __FILE__ ) . '/home.php';
+error_reporting(MARSBLE_DEBUG ? E_ALL | E_STRICT : 0);
 
+if (is_home()) {
+    require __DIR__ . '/home.php';
 } else {
-
-    require dirname(  __FILE__ ) . '/class-favicons.php';
-
-    $favicon = new Favicons;
-    $url = removehttp($_SERVER['REQUEST_URI']);
-
-    echo $favicon->getFavicon($url);
-
+    require __DIR__ . '/class-favicons.php';
+    $favicon = new Favicons(MARSBLE_URL_PATH);
+    // `http://127.0.0.1/example.com?cache=false`
+    if (isset($_GET['cache']) && !$_GET['cache']) {
+        $favicon->expires = 0;
+    }
+    $favicon->debugMode = MARSBLE_DEBUG;
+    $favicon->draw();
 }
